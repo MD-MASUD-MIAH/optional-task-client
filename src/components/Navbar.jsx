520993dcc893d9d75047b4020732f5ab04f7b573
176409 <148116817+176409@users.noreply.github.com>
2025-05-19 12:11:16 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router';
 
 const Navbar = () => {
     return (
-        <div className='flex gap-12 justify-center items-center  w-11/12 mx-auto  '>
+        <div className='flex gap-12 py-10 justify-center items-center  w-11/12 mx-auto  '>
             <NavLink to='/'>Home</NavLink>
             <NavLink to='/adduser'>Adduser</NavLink>
          
